d087bcbbb230fa314c56d154ae491bfc6407b6e5
ryota-aus <74895872+ryota-aus@users.noreply.github.com>
2020-12-21 18:36:56 +0900

    商品出品機能2回目修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:description,:price,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:delivery_day_id,:numericality).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name,:description,:price,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:delivery_day_id,:numericality).merge(user_id: current_user.id)
   end
 
   
