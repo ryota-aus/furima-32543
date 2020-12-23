64e39f999b3a3fc45ad697030a092b0c81fb91fb
ryota-aus <74895872+ryota-aus@users.noreply.github.com>
2020-12-23 17:16:06 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only:[:show, :edit, :update]
+
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -17,11 +19,23 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  #def edit
-    
-  #end
+  
   def show
-    @item = Item.find(params[:id])
+   
+  end
+
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
 
@@ -30,6 +44,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image,:name,:description,:price,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:delivery_day_id,:numericality).merge(user_id: current_user.id)
   end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
  
 
 end
