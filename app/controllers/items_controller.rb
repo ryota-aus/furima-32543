73e663f75f0ac45b9e07f6ab3a18fc3578d7d6cf
ryota-aus <74895872+ryota-aus@users.noreply.github.com>
2020-12-23 11:21:58 +0900

    商品詳細機能表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:edit, :show]
   def index
     @items = Item.all.order("created_at DESC")
  
@@ -16,13 +17,22 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+  def edit
+    
+  end
+  def show
+    
+  end
+
 
   private
 
   def item_params
     params.require(:item).permit(:image,:name,:description,:price,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:delivery_day_id,:numericality).merge(user_id: current_user.id)
   end
-
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
 
 end
